2eb09ee28103a25b87c7dc3ca5bc25b0ed8d0b6a
Umar-codingal <umarfarukpatelkp@gmail.com>
2025-01-10 09:52:42 +0530

    module

--- Exeption handling/byeBye.py
+++ Exeption handling/byeBye.py
@@ -1,9 +1,13 @@
-valid = False
-while not valid :
-    n=int(input("Enter a number:"))
-    while n%2==0:
-        print("Bye")
+# valid = False
+# while not valid :
+#     n=int(input("Enter a number:"))
+#     while n%2==0:
+#         print("Bye")
       
     
     
-print("valid")
+# print("valid")
+
+import math
+
+print(ceil(98))
